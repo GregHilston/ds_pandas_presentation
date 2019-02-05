77584c9feb131023e7acc18f327c16b4847f0546
GregHilston <Gregory.Hilston@gmail.com>
2019-02-05 12:32:00 -0700

    Fixed copy and paste error

--- validate.py
+++ validate.py
@@ -9,7 +9,7 @@ validation_df = df = pd.read_csv(INPUT_FILE)
 
 def validate(expected, actual, message):
     print(f"expected type {type(expected)}")
-    print(f"expectactualed type {type(actual)}")
+    print(f"actual type {type(actual)}")
 
     if ((isinstance(expected, pd.DataFrame) or isinstance(expected, pd.Series)) and not expected.equals(actual)) or ((isinstance(expected, float) or isinstance(expected, int) or isinstance(expected, str)) and expected != actual):
         print("Incorrect!")
